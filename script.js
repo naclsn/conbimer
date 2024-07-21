9437b9929599338935c3b2577df63b0fa275ead0
Grenier Celestin <celestin.grenier@hotmail.com>
2024-07-21 15:47:31 +0200

    alphaishmaybe

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ window.onload = async function() {
     /** @type {HTMLElement} */ const known = window.known;
     /** @type {HTMLElement} */ const colors = window.colors;
     /** @type {HTMLElement} */ const colmixer = window.colmixer;
+    /** @type {HTMLInputElement} */ const mix_ratio = window.mix_ratio;
+    /** @type {HTMLInputElement} */ const mix_white = window.mix_white;
+    /** @type {HTMLButtonElement} */ const mix_done = window.mix_done;
     /** @type {HTMLCanvasElement} */ const drawing = window.drawing;
 
     drawing.width = drawing.height = innerWidth;
@@ -39,10 +42,8 @@ window.onload = async function() {
         it.onpointerdown = knownItemPointerDown;
     }
 
-    known_number.textContent = known.childElementCount.toString();
-
-    clear_items.onclick = _ => fetch('clr');
-    update_files.onclick = _ => fetch('upd');
+    clear_items.onclick =  _ => fetch('clr').then(() => alert("c fai")).catch(() => alert("peu pa"));
+    update_files.onclick = _ => fetch('upd').then(() => alert("c fai")).catch(() => alert("peu pa"));
 
     boop.onclick = goDraw;
     draw_undo.onclick = drawUndo;
@@ -77,25 +78,31 @@ window.onload = async function() {
 
         const rect = arena.getBoundingClientRect();
         if (rect.top < ev.y && ev.y < rect.bottom) {
-            const cols = target.getAttribute('data-colors').split('-');
-            const linked = new Array(cols.length);
-            for (let k = 0; k < cols.length; ++k) if (!availColorSet.has(cols[k])) {
-                availColorSet.add(cols[k]);
-                const it = linked[k] = colors
-                    .appendChild(document.createElement('li'))
-                    .appendChild(document.createElement('div'));
-                it.setAttribute('style', '--color: #'+cols[k]+';');
-                it.setAttribute('class', 'color');
-                it.onclick = takePenPicker;
+            if (!itemColorMap.has(target)) {
+                const cols = target.getAttribute('data-colors').split('-');
+                const linked = [];
+                for (let k = 0; k < cols.length; ++k) if (!availColorSet.has(cols[k])) {
+                    availColorSet.add(cols[k]);
+                    const it = colors
+                        .appendChild(document.createElement('li'))
+                        .appendChild(document.createElement('div'));
+                    it.setAttribute('style', '--color: #'+cols[k]+';');
+                    it.setAttribute('class', 'color');
+                    it.onclick = takePenPicker;
+                    linked.push(it);
+                }
+                itemColorMap.set(target, linked);
+                if (1 < arena.childElementCount) boop.removeAttribute('disabled');
             }
-            itemColorMap.set(target, linked);
-            if (1 < arena.childElementCount) boop.removeAttribute('disabled');
         }
 
         else {
-            for (const it of itemColorMap.get(target) || []) it.remove();
-            itemColorMap.delete(target);
             target.remove();
+            for (const it of itemColorMap.get(target) || []) {
+                availColorSet.delete(it.getAttribute('style').slice('--color: #'.length, -';'.length));
+                it.remove();
+            }
+            itemColorMap.delete(target);
             if (arena.childElementCount < 2) boop.setAttribute('disabled', 'oui');
         }
 
@@ -124,14 +131,11 @@ window.onload = async function() {
 
     const penpicker = document.createElement('div');
     penpicker.setAttribute('id', 'penpicker');
-    if (1) {
-        const mixl = penpicker.appendChild(document.createElement('span'));
-        mixl.setAttribute('style', 'float: left;color: #000;');
-        mixl.onclick = mixCols;
-        const mixr = penpicker.appendChild(document.createElement('span'));
-        mixr.setAttribute('style', 'float: right;color: #000;');
-        mixr.onclick = mixCols;
-    }
+    const pick_mixl = penpicker.appendChild(document.createElement('span'));
+    pick_mixl.textContent = '<+';
+    const pick_mixr = penpicker.appendChild(document.createElement('span'));
+    pick_mixr.setAttribute('style', 'float: right;color: #000;');
+    pick_mixr.textContent = '+>';
     for (const size of [innerWidth/40, innerWidth/12, innerWidth/7]) {
         const pen = penpicker.appendChild(document.createElement('div'));
         pen.setAttribute('class', 'pensize');
@@ -140,28 +144,90 @@ window.onload = async function() {
     }
 
     function takePenPicker(/** @type {PointerEvent} */ ev) {
+        ev.stopPropagation();
         /** @type {HTMLElement} */ const col = ev.target;
         col.appendChild(penpicker);
+        if (col.parentElement.previousElementSibling) {
+            pick_mixl.onclick = mixCols;
+            pick_mixl.setAttribute('style', 'float: left;color: #000;');
+        } else pick_mixl.setAttribute('style', 'float: left;color: #fff;');
+        if (col.parentElement.nextElementSibling) {
+            pick_mixr.onclick = mixCols;
+            pick_mixr.setAttribute('style', 'float: right;color: #000;');
+        } else pick_mixr.setAttribute('style', 'float: right;color: #fff;');
     }
 
     function pickPen(/** @type {PointerEvent} */ ev) {
+        ev.stopPropagation();
         /** @type {HTMLElement} */ const pen = ev.target;
-        const col = penpicker.parentNode;
+        const col = penpicker.parentElement;
         ctx.strokeStyle = col.getAttribute('style').slice('--color: '.length, -';'.length);
         ctx.lineWidth = parseInt(pen.getAttribute('style').slice('--size: '.length, -'px;'.length));
         col.removeChild(penpicker);
     }
 
+    /** @type {[number, number, number, number]} */
+    let mixed1;
+    /** @type {[number, number, number, number]} */
+    let mixed2;
+    /** @type {HTMLElement} */
+    let mixel1;
+    /** @type {HTMLElement} */
+    let mixel2;
+
     function mixCols(/** @type {PointerEvent} */ ev) {
+        ev.stopPropagation();
+        mix_ratio.value = 50;
+        mix_white.value = 100;
         /** @type {HTMLElement} */ const dir = ev.target;
-        colmixer.removeAttribute('style');
+        const left = dir === penpicker.firstElementChild;
+        if (left) {
+            mixed1 = cmyk(mixel1 = penpicker.parentElement);
+            mixed2 = cmyk(mixel2 = mixel1.parentElement.previousElementSibling.firstElementChild);
+        } else {
+            mixed2 = cmyk(mixel2 = penpicker.parentElement);
+            mixed1 = cmyk(mixel1 = mixel2.parentElement.nextElementSibling.firstElementChild);
+        }
+        penpicker.remove();
+        updateMix();
+        function cmyk(/** @type {HTMLElement} */ node) {
+            const txt = node.getAttribute('style').slice('--color: #'.length, -';'.length);
+            const rgb = [0, 2, 4].map(k => parseInt(txt.slice(k, k+2), 16));
+            const r = rgb[0]/255, g = rgb[1]/255, b = rgb[2]/255, k = 1-Math.max(r, g, b);
+            return [(1-r-k) / (1-k), (1-g-k) / (1-k), (1-b-k) / (1-k), k];
+        }
     }
 
+    mix_ratio.onchange = mix_white.onchange = updateMix;
+
+    function updateMix() {
+        const t = mix_ratio.value/100, w = mix_white.value/100;
+        const cmyk = mixed1.map((_, k) => Math.max(0, Math.min(1,
+            (t*mixed1[k] + (1-t)*mixed2[k]) / w
+        )));
+        const color = [
+            255*(1-cmyk[0])*(1-cmyk[3]) |0,
+            255*(1-cmyk[1])*(1-cmyk[3]) |0,
+            255*(1-cmyk[2])*(1-cmyk[3]) |0,
+        ].map(c => c.toString(16).padStart(2, '0')).join('');
+        colmixer.setAttribute('style', '--color: #'+color+';');
+    };
+
+    mix_done.onclick = (/** @type {PointerEvent} */ ev) => {
+        ev.stopPropagation();
+        const color = colmixer.getAttribute('style');
+        mixel1.setAttribute('style', color);
+        mixel2.parentElement.remove();
         colmixer.setAttribute('style', 'visibility: hidden;');
+        ctx.strokeStyle = color.slice('--color: '.length, -';'.length);
+    };
 
     document.body.onpointerdown = (/** @type {PointerEvent} */ ev) => {
-        if (ev.target.parentNode !== penpicker && penpicker.parentNode) {
-            penpicker.parentNode.removeChild(penpicker);
+        if (ev.target.parentElement !== penpicker && penpicker.parentElement) {
+            penpicker.parentElement.removeChild(penpicker);
+            ev.stopPropagation();
+        } else if (colmixer.getAttribute('style').startsWith('--color') && !colmixer.contains(ev.target)) {
+            colmixer.setAttribute('style', 'visibility: hidden;');
             ev.stopPropagation();
         }
     };
@@ -173,7 +239,7 @@ window.onload = async function() {
         for (const one of arena.children) arenachld.push(one.src.slice(one.src.indexOf('=')+1));
         arenachld.sort((a, b) => a-b);
 
-        arena.innerHTML = ''; // XXX: *poof* :(
+        arena.innerHTML = '';
         draggingPointers.length = 0;
         itemColorMap.clear();
         availColorSet.clear();
@@ -191,6 +257,7 @@ window.onload = async function() {
         ctx.lineWidth = innerWidth/24;
     }
 
+    setTimeout(() => known_number.textContent = known.childElementCount.toString(), 1000);
     async function goDone(/** @type {boolean} */ done) {
         colors.innerHTML = '';
         for (const button of button_grp.children) button.setAttribute('disabled', 'oui');
@@ -209,7 +276,7 @@ window.onload = async function() {
             const collst = []; colset.forEach(v => collst.push(v));
             const usedcolors = collst.join('-');
             const madefrom = arenachld.join('-');
-            fetch('put?i='+nameas, {
+            await fetch('put?i='+nameas, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'image/png',
@@ -217,20 +284,24 @@ window.onload = async function() {
                     'X-Made-From': madefrom,
                 },
                 body: await resizer.convertToBlob(),
-            }).then(_ => {
-                const it = known
-                    .appendChild(document.createElement('li'))
-                    .appendChild(document.createElement('img'));
-                it.setAttribute('class', 'item');
-                it.src = 'get?i='+nameas;
-                it.setAttribute('data-colors', usedcolors);
-                it.onpointerdown = knownItemPointerDown;
             });
+            const it = known
+                .appendChild(document.createElement('li'))
+                .appendChild(document.createElement('img'));
+            it.setAttribute('class', 'item');
+            it.src = 'get?i='+nameas;
+            it.setAttribute('data-colors', usedcolors);
+            it.onpointerdown = knownItemPointerDown;
+            known_number.textContent = known.childElementCount.toString();
+
         }
     }
 
     function drawUndo() {
-        if (!--last_drawn) draw_undo.setAttribute('disabled', 'oui');
+        if (!--last_drawn) {
+            draw_undo.setAttribute('disabled', 'oui');
+            draw_done.setAttribute('disabled', 'oui');
+        }
         draw_redo.removeAttribute('disabled');
 
         ctx.clearRect(0, 0, innerWidth, innerWidth);
@@ -248,6 +319,7 @@ window.onload = async function() {
     function drawRedo() {
         if (drawn.length === ++last_drawn) draw_redo.setAttribute('disabled', 'oui');
         draw_undo.removeAttribute('disabled');
+        draw_done.removeAttribute('disabled');
 
         const last = drawn[last_drawn-1];
         ctx.strokeStyle = last[0];
